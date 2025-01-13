d88e845175c4c6d9730f89f1e046a30baef56809
Ketan <kdeolikar17@gmail.com>
2025-01-13 15:12:35 +0530

    Project is working

---
+++ src/test/java/org/testingacademy/tests/TestSample.java
@@ -0,0 +1,20 @@
+package org.testingacademy.tests;
+
+import io.qameta.allure.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSample {
+
+    @Description("Sample TC 1")
+    @Test
+    public void testSample0(){
+        Assert.assertTrue(true);
+    }
+
+    @Description("Sample TC 1")
+    @Test
+    public void testSample1(){
+        Assert.assertTrue(true);
+    }
+}
